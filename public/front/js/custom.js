df33b22dc07a6f09d3f7b53bf068843e871cb3ca
vkishan994 <kishanvaghela994@gmail.com>
2025-11-27 18:02:42 +0530

    initial commit

---
+++ public/front/js/custom.js
@@ -0,0 +1,20 @@
+const link = document.getElementById('style');
+link.href = `css/style.css?v=${new Date().getTime()}`;
+
+$(document).ready(function () {
+    $('.testimonial-carousel').slick({
+        dots: false,
+        infinite: true,
+        speed: 600,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: true,
+        fade: false,
+        cssEase: 'ease-in-out',
+        pauseOnHover: true,
+        pauseOnFocus: true,
+        adaptiveHeight: true
+    });
+});
---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->middleware(['auth', 'verified'])->name('home');
